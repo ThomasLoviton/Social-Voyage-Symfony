83570eb47b6cb7dac58d693893dae252e26e97f5
unknown <thomasloviton@gmail.com>
2019-02-05 23:13:36 +0100

    list(), show(), create(), ~edit()

---
+++ src/Controller/PostController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Post;
+
+/**
+ * @Route("/post", name="post")
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="post_list")
+     */
+    public function list()
+    {
+        $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
+
+        // Pour retourner en JSON -> composer require symfony/serializer-pack
+        return $this->json($posts);
+    }
+
+    /**
+     * @Route("/show/{id}", name="post_show")
+     */
+    public function show($id)
+    {
+        $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
+
+        return $this->json($post);
+    }
+
+    /**
+     * @Route("/new", name="post_create")
+     */
+    public function create(Request $request)
+    {
+        $date = new \Datetime();
+        // Configuration PHP -> changer date.timezone
+        // OU $date->setTimezone(new \DateTimeZone('Europe/Paris'));
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $post = new Post();
+        $post->setTitle($request->get('title'));
+        $post->setShorttext($request->get('shorttext'));
+        $post->setText($request->get('text'));
+        $post->setAuthor($request->get('author'));
+        $post->setUrlimage($request->get('urlimage'));
+        $post->setCreatedat($date);
+        $post->setUpdatedat($date);
+
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return $this->json($post);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="post_edit", methods={"POST"})
+     */
+    public function update($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $post = $entityManager->getRepository(Post::class)->find($id);
+
+        
+        $entityManager->flush();
+
+        return $this->json($post);
+    }
+}
---
+++ src/Entity/Post.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table(name="post")
+ * @ORM\Entity
+ */
+class Post
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, nullable=false)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="shorttext", type="string", length=255, nullable=false)
+     */
+    private $shorttext;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text", length=0, nullable=false)
+     */
+    private $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255, nullable=false)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="urlimage", type="string", length=255, nullable=false)
+     */
+    private $urlimage;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdat", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
+     */
+    private $createdat;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updatedat", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
+     */
+    private $updatedat;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getShorttext(): ?string
+    {
+        return $this->shorttext;
+    }
+
+    public function setShorttext(string $shorttext): self
+    {
+        $this->shorttext = $shorttext;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getUrlimage(): ?string
+    {
+        return $this->urlimage;
+    }
+
+    public function setUrlimage(string $urlimage): self
+    {
+        $this->urlimage = $urlimage;
+
+        return $this;
+    }
+
+    public function getCreatedat(): ?\DateTimeInterface
+    {
+        return $this->createdat;
+    }
+
+    public function setCreatedat(\DateTimeInterface $createdat): self
+    {
+        $this->createdat = $createdat;
+
+        return $this;
+    }
+
+    public function getUpdatedat(): ?\DateTimeInterface
+    {
+        return $this->updatedat;
+    }
+
+    public function setUpdatedat(\DateTimeInterface $updatedat): self
+    {
+        $this->updatedat = $updatedat;
+
+        return $this;
+    }
+
+
+}
